21fa7c8a7e9bf51bc0188d1baa3bca322887a591
tomBlt <bouillottom@gmail.com>
2025-12-08 13:37:51 +0100

    base propre anglais

--- pc-clicker/src/app/app.routes.ts
+++ pc-clicker/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { Clicker } from './clicker/clicker';
 import {Homepage} from './homepage/homepage';
 
 export const routes: Routes = [
-  {path: 'clicker', component: Clicker},
   {path : '', component: Homepage},
 ];
--- pc-clicker/src/app/app.ts
+++ pc-clicker/src/app/app.ts
@@ -1,7 +1,5 @@
 import { Component, signal } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import { LogiqueJeu } from './logique-jeu/logique-jeu';
-import {LogiqueJeuService} from './logique-jeu/logique-jeu-service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +8,4 @@ import {LogiqueJeuService} from './logique-jeu/logique-jeu-service';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('pc-clicker');
-  constructor(private logiqueService: LogiqueJeuService) {
-    console.log("Le jeu est initialisé !");
-  }
 }
