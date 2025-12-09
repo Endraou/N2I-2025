46afb0191b4cb38c9a62f731549ddd7209446ea9
Nic0lasmalay <nicolas.malay@gmail.com>
2025-12-09 17:34:35 +0100

    enigme done

--- pc-clicker/src/app/app.routes.ts
+++ pc-clicker/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {Homepage} from './homepage/homepage';
+import {EnglishNicoComponent} from './components/enigmEnglishNico/enigmEnglishNico';
 
 export const routes: Routes = [
   {path : '', component: Homepage},
+  {path:'enigmEnglishNico', component: EnglishNicoComponent}
 ];
